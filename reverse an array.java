edffc85ad2a7816f106da8a8dc23c456427fee51
arunthatiparthi <127715461+arunthatiparthi@users.noreply.github.com>
2023-11-04 10:17:18 +0530

    Add files via upload

---
+++ reverse an array.java
@@ -0,0 +1,17 @@
+class a{
+    public static void main(arg[] string){
+    int a[]={1,2,3,4,5,6};
+    int i=0; j=a-1; temp;
+    while(i<j){
+    a[i]=temp;
+    a[i]=a[j];
+    a[j]=temp;
+    i++;
+    j--;
+    }
+    System.out.println(array(a));
+
+    }
+    
+
+}
